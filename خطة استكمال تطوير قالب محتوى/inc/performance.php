5d19fad0eac06d1b09bd683c11966c20a67c9fcf
sammanfadi <sammanfadi3@gmail.com>
2025-08-03 01:34:22 +0300

    Update performance.php

--- خطة استكمال تطوير قالب محتوى/inc/performance.php
+++ خطة استكمال تطوير قالب محتوى/inc/performance.php
@@ -996,6 +996,21 @@ class MuhtawaaPerformance {
             return $data;
         }, 10, 4);
     }
+    
+    /**
+     * تنظيف ووردبريس من الميزات غير المطلوبة
+     */
+    public static function cleanup_wordpress() {
+        // إزالة روابط غير ضرورية من الهيدر
+        remove_action('wp_head', 'rsd_link');
+        remove_action('wp_head', 'wlwmanifest_link');
+        remove_action('wp_head', 'wp_generator');
+        remove_action('wp_head', 'feed_links', 2);
+        remove_action('wp_head', 'feed_links_extra', 3);
+        remove_action('wp_head', 'adjacent_posts_rel_link_wp_head');
+        remove_action('wp_head', 'wp_shortlink_wp_head');
+        // أضف أي تنظيفات أخرى تحتاجها هنا
+    }
 }
 
 // تهيئة تحسينات الأداء
